f6fe76bb21a37d7f9d63abaee761a108d30aa2f0
Owen Leonard <owenleonard@gmail.com>
2014-06-10 23:51:47 -0600

    clean up html structure and associated css

--- website/models.py
+++ website/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class SubService(models.Model):
     title = models.CharField(max_length=256)
-    
-    def __unicode__(self):
+
+    def __str__(self):
         return self.title
 
 class Service(models.Model):
     title = models.CharField(max_length=128)
     subservices = models.ManyToManyField(SubService)
     display = models.BooleanField(default=False)
-    
-    def __unicode__(self):
+
+    def __str__(self):
         return self.title
