f8bc9511dcb3ffc5b50315bcd8e3d22c2beec7f3
Owen Leonard <owenleonard@gmail.com>
2014-07-30 21:21:17 -0600

    clean out unused variable

--- website/views.py
+++ website/views.py
@@ -8,24 +8,24 @@ def events():
 
 # Create your views here.
 def index(request):
-    return render_to_response('website/index.html', {'page': 'index', 'header': '', 'events': events()})
+    return render_to_response('website/index.html', {'header': '', 'events': events()})
 
 def drbetty(request):
     services = Service.objects.filter(display=True)
-    return render_to_response('website/drbetty.html', {'page': 'drbetty', 'header': 'Dr. Betty', 'services': services, 'events': events()})
+    return render_to_response('website/drbetty.html', {'header': 'Dr. Betty', 'services': services, 'events': events()})
 
 def health(request):
-    return render_to_response('website/health.html', {'page': 'health', 'header': 'Health', 'events': events()})
+    return render_to_response('website/health.html', {'header': 'Health', 'events': events()})
 
 def sports(request):
-    return render_to_response('website/sports.html', {'page': 'sports', 'header': 'Sports', 'events': events()})
+    return render_to_response('website/sports.html', {'header': 'Sports', 'events': events()})
 
 def lifecoach(request):
-    return render_to_response('website/lifecoach.html', {'page': 'lifecoach', 'header': 'Life Coach', 'events': events()})
+    return render_to_response('website/lifecoach.html', {'header': 'Life Coach', 'events': events()})
 
 def downloads(request):
-    return render_to_response('website/downloads.html', {'page': 'downloads', 'header': 'Downloads', 'events': events()})
+    return render_to_response('website/downloads.html', {'header': 'Downloads', 'events': events()})
 
 def event_details(request, event_id):
     event = get_object_or_404(Event, pk=event_id, display=True)
-    return render_to_response('website/event.html', {'page': 'event', 'header': 'Event', 'events': events(), 'event': event})
+    return render_to_response('website/event.html', {'header': 'Event', 'events': events(), 'event': event})
